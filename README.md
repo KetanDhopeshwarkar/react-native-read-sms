9e7e0d6114958db1a6a60e6730c8175466e03f9f
Ketan Dhopeshwarkar <ketan@biz4solutions.com>
2020-04-10 18:13:22 +0530

    Update README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # React native plugin for Read SMS
 	This plugin is used to read new received SMS.
-	Supported and tested up to React Native V 0.61.5
+	Supported and tested up to React Native V0.61.5
 
 ## Getting started
 
@@ -13,8 +13,33 @@
 `$ react-native link react-native-read-sms`
 
 ## Usage
-```javascript
-import ReadSms from 'react-native-read-sms';
 
-ReadSms;
+To enable `Read SMS` feature you have to add the following code to the `AndroidManifest.xml`:
+
+```java
+  <uses-permission android:name="android.permission.RECEIVE_SMS" />
+  <uses-permission android:name="android.permission.READ_SMS" />
+```
+
+```javascript
+import * as ReadSms from 'react-native-read-sms/ReadSms';
+
+/*
+* use to start read new recived messages
+*/
+ReadSms.startReadSMS((status, sms, error) => {
+	if (status == "success") {
+		this.extractNumber(sms);
+	}
+});
+
+/*
+* use to stop read messages
+*/
+ReadSms.stopReadSMS();
+
+/*
+* use to request Read SMS Permission
+*/
+ReadSms.requestReadSMSPermission();
 ```
---
+++ ReadSms.js
@@ -0,0 +1,64 @@
+import { NativeEventEmitter, NativeModules, PermissionsAndroid, Platform } from "react-native";
+
+
+export async function startReadSMS(callback) {
+    let resultFun = (status, sms, error) => {
+        if (callback) {
+            callback(status, sms, error);
+        }
+    }
+    if (Platform.OS === 'android') {
+        const hasPermission = await hasSMSPermission();
+        if (hasPermission) {
+            NativeModules.ReadSms.startReadSMS(result => {
+                new NativeEventEmitter(NativeModules.ReadSms)
+                    .addListener('received_sms', (sms) => {
+                        resultFun("success", sms);
+                    });
+            }, error => {
+                resultFun("error", '', error);
+            });
+        } else {
+            resultFun("error", '', "Required RECEIVE_SMS and READ_SMS permission");
+        }
+    } else {
+        resultFun("error", '', "ReadSms Plugin is only for android platform");
+    }
+}
+
+hasSMSPermission = async () => {
+    if (Platform.OS === 'android' && Platform.Version < 23) {
+        return true;
+    }
+    const hasReceiveSmsPermission = await PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.RECEIVE_SMS
+    );
+    const hasReadSmsPermission = await PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.READ_SMS
+    );
+    if (hasReceiveSmsPermission && hasReadSmsPermission) return true;
+    return false;
+}
+
+export async function requestReadSMSPermission() {
+    if (Platform.OS === 'android') {
+        const hasPermission = await hasSMSPermission();
+        if (hasPermission) return true;
+        const status = await PermissionsAndroid.requestMultiple(
+            [PermissionsAndroid.PERMISSIONS.RECEIVE_SMS, PermissionsAndroid.PERMISSIONS.READ_SMS]
+        );
+        if (status === PermissionsAndroid.RESULTS.GRANTED) return true;
+        if (status === PermissionsAndroid.RESULTS.DENIED) {
+            console.log('Read Sms permission denied by user.', status);
+        } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+            console.log('Read Sms permission revoked by user.', status);
+        }
+        return false;
+    }
+}
+
+export function stopReadSMS() {
+    if (Platform.OS === 'android') {
+        NativeModules.ReadSms.stopReadSMS();
+    }
+}
